39c274430ee6c2f2b14edcaafcfaf611ce6f9420
wolfpirker <w_pirker@gmx.de>
2021-04-05 12:29:35 +0200

    completed dice app

--- DiceApp/app/src/main/java/com/wolfgang/diceapp/MainActivity.kt
+++ DiceApp/app/src/main/java/com/wolfgang/diceapp/MainActivity.kt
@@ -3,29 +3,44 @@ package com.wolfgang.diceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 // Note: in classes: CamelCase writing by convention
 class MainActivity : AppCompatActivity() {
 
     // Note: findViewById cannot be called here, before it is inizialized -> App crashs otherwise!
-    lateinit var randomNumberTxt : TextView
+    lateinit var diceOneImg : ImageView
+    lateinit var diceTwoImg : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        randomNumberTxt = findViewById<TextView>(R.id.text_view_random_number)
+        diceOneImg = findViewById(R.id.dice_one)
+        diceTwoImg = findViewById(R.id.dice_two)
+
         val rollButton = findViewById<Button>(R.id.btn_roll_dice)
 
         rollButton.setOnClickListener {
-            rollDice()
+            rollDice(diceOneImg)
+            rollDice(diceTwoImg)
         }
     }
 
     // Note convention: first word lower case; 2nd 1st letter uppercase
-    fun rollDice(){
+    fun rollDice(imgView: ImageView){
         val randomNumber = (1..6).random()
-        randomNumberTxt.text = randomNumber.toString()
+
+        val image = when (randomNumber){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            6 -> R.drawable.dice6
+            else -> R.drawable.dice_empty
+        }
+        imgView.setImageResource(image)
     }
 }
