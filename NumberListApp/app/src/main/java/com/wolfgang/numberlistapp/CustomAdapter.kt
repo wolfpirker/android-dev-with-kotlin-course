a5d4435ede6f2dcbf38c1d324ee605bb5d410aa4
wolfpirker <w_pirker@gmx.de>
2021-05-09 00:46:04 +0200

    use mediaPlayer to play audio files

--- NumberListApp/app/src/main/java/com/wolfgang/numberlistapp/CustomAdapter.kt
+++ NumberListApp/app/src/main/java/com/wolfgang/numberlistapp/CustomAdapter.kt
@@ -1,10 +1,12 @@
 package com.wolfgang.numberlistapp
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
 
@@ -27,6 +29,16 @@ class CustomAdapter(var arrayList: ArrayList<Data>) : BaseAdapter() {
 
         val numberTextView = rowView.findViewById<TextView>(R.id.number_text_view)
         numberTextView.text = item.number
+
+        val audioImageView = rowView.findViewById<ImageView>(R.id.audio_image_view)
+        audioImageView.setOnClickListener {
+            val mediaPlayer = MediaPlayer.create(
+                context,
+                context.resources.getIdentifier(
+                    item.audioFileName, "raw", context.packageName)
+            )
+            mediaPlayer.start()
+        }
 
         return rowView
     }
