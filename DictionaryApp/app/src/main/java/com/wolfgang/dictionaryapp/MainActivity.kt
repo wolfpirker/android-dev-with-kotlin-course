51d30cbd7aae900eea180d65c2c6c8b140c13ab2
wolfpirker <w_pirker@gmx.de>
2021-05-15 14:24:54 +0200

    Dictionary app, quering json, http connection and internet access

--- DictionaryApp/app/src/main/java/com/wolfgang/dictionaryapp/MainActivity.kt
+++ DictionaryApp/app/src/main/java/com/wolfgang/dictionaryapp/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.wolfgang.dictionaryapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.wolfgang.dictionaryapp.databinding.ActivityMainBinding
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,7 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     // JSON Request
     // https://www.dictionaryapi.com/api/v3/references/learners/json/
-    // apple?key=your-api-key
+    // apple?key=2c915c85-1a95-4247-b97a-21437a92a82d
+
+    private val KEY = "WORD_DEFINITION" // -> contanst in upper case!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +29,48 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val queue = Volley.newRequestQueue(this)
+        binding.findButton.setOnClickListener {
+            val word = binding.wordEditText.text
+            val apiKey = "2c915c85-1a95-4247-b97a-21437a92a82d"
+            val url = getUrl()
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                {response ->
+                    try {
+                        extractDefinitionFromJson(response)
+                    }catch (exception : Exception){
+                        exception.printStackTrace()
+                    }
+                },
+                {error ->
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                }
+            )
+            queue.add(stringRequest)
+        }
+
     }
+
+    private fun getUrl() : String{
+        val word = binding.wordEditText.text
+        val apiKey = "2c915c85-1a95-4247-b97a-21437a92a82d"
+        val url =
+            "https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
+
+        return url
+    }
+
+    private fun extractDefinitionFromJson(response : String){
+
+        val jsonArray = JSONArray(response)
+        val firstIndex = jsonArray.getJSONObject(0) // first index of that array
+        val getShortDefinition = firstIndex.getJSONArray("shortdef")
+        val firstShortDefinition = getShortDefinition.get(0) // first index
+
+        // to navigate from the one activity to the other one; we navigate to WordDefinitionActivity
+        val intent = Intent(this, WordDefinitionActivity::class.java)
+        intent.putExtra(KEY, firstShortDefinition.toString()) // pass the information to the new activity
+        startActivity(intent)
+    }
+
 }
--- DictionaryApp/app/src/main/java/com/wolfgang/dictionaryapp/WordDefinitionActivity.kt
+++ DictionaryApp/app/src/main/java/com/wolfgang/dictionaryapp/WordDefinitionActivity.kt
@@ -3,11 +3,23 @@ package com.wolfgang.dictionaryapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.wolfgang.dictionaryapp.databinding.ActivityDefinitionBinding
 
 class WordDefinitionActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDefinitionBinding
+    private val KEY = "WORD_DEFINITION"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_definition)
+
+        binding = ActivityDefinitionBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_definition)
+        val view = binding.root
+        setContentView(view)
+
+        binding.txtvDefinition.text = intent.getStringExtra(KEY)
+
+        binding.imgBack.setOnClickListener { finish() }
     }
 
     fun clickBackwardArrow(view: View) {}
